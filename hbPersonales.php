4917d6da67b2aacc063b5d61dd0b120b38a508cb
Johan-Ortega118 <85240547+Johan-Ortega118@users.noreply.github.com>
2023-03-15 18:02:01 -0600

    Update hbPersonales.php

--- hbPersonales.php
+++ hbPersonales.php
@@ -22,10 +22,11 @@ session_start();
     ?>
    
     <!--</menu>-->
-    <div class="container px-4 px-lg-5">
+    <main class="main">
+        <div class="container px-4 px-lg-5 bg-white">
             <!-- Heading Row-->
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
-                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="img/hbPersonales.jpeg"  alt="..." /></div>
+                <div class="col-lg-7"><img class="img-fluid shadow rounded mb-4 mb-lg-0" src="img/hbPersonales.jpeg"  alt="..." /></div>
                 <div class="col-lg-5">
                     <h1 class="font-weight-light">Test de Habilidades personales</h1>
                     <p>Al realizar las siguientes encuestas usted obtendrá un resultado para identificar áreas de oportunidad.</p>
@@ -43,9 +44,9 @@ session_start();
                             <h2 class="card-title">Desarrollo del Auto conocimiento</h2>
                             <p class="card-text">-Evaluación de la inteligencia emocional<br>
                                                 -Determinación de valores y prioridades<br>
--Identificación del estilo cognoscitivo<br>
--Evaluación de la actitud hacia el cambio<br>
--Comprensión de la autoevaluación básica y esencial</p>
+                                                -Identificación del estilo cognoscitivo<br>
+                                                -Evaluación de la actitud hacia el cambio<br>
+                                                -Comprensión de la autoevaluación básica y esencial</p>
                         </div>
                         <div class="card-footer"><a class="btn btn-primary btn-sm" href="EncuestaSolAnaCrea.php">Empezar</a></div>
                     </div>
@@ -55,8 +56,8 @@ session_start();
                         <div class="card-body">
                             <h2 class="card-title">Evaluación del Manejo del Estrés</h2>
                             <p class="card-text">-Cómo enfrentar los factores estresantes<br>
--Administración del tiempo<br>
--Desarrollo de elasticidad</p>
+                                                -Administración del tiempo<br>
+                                                -Desarrollo de elasticidad</p>
                         </div>
                         <div class="card-footer"><a class="btn btn-primary btn-sm" href="EncuestaEstres.php">Comenzar</a></div>
                     </div>
@@ -66,14 +67,15 @@ session_start();
                         <div class="card-body">
                             <h2 class="card-title">Solución Creativa de Problemas</h2>
                             <p class="card-text">-Uso del enfoque racional<br>
--Uso del enfoque creativo<br>
--Fomento de la innovación en los demás</p>
+                                                -Uso del enfoque creativo<br>
+                                                -Fomento de la innovación en los demás</p>
                         </div>
                         <div class="card-footer"><a class="btn btn-primary btn-sm" href="EncuestaSolAnaCrea.php">Empezar</a></div>
                     </div>
                 </div>
             </div>
         </div>
+    </main>
             <!-- Footer Links -->
             <!-- Copyright -->
             <div class="footer-copyright text-center py-3 text-secondary" style="background-color:#000000;">© 2020 Copyright: MDBootstrap.com
